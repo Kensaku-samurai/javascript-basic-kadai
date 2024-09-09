4d17a7d4bca930fe23f8989063c9c14c61fbeed3
Kensaku-samurai <kensaku155@outlook.jp>
2024-09-09 11:24:57 +0900

    mdn.html

--- mdn.html/mdn.js
+++ mdn.html/mdn.js
@@ -1,4 +1,18 @@
-const today = new Date();
+const today = () => {
+    const today = new Date();
+    console.log(today.getFullYear());
+    console.log(today.getMonth() + 1);
+    console.log(today.getDate());
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    const date = currentDate.getDate();
+    const currenDate =new Date();
+    const formatteDate = '${year}年${month}月&{date}日';
+    console.log(formatteDate);
 
- 
-console.log(today);
+};
+
+today();
+
+
