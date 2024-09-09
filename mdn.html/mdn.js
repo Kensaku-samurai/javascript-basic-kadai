0c4fccf0ca1c93e346741aa48adb438a0e2fe979
Kensaku-samurai <kensaku155@outlook.jp>
2024-09-09 11:37:15 +0900

    mdn.html

--- mdn.html/mdn.js
+++ mdn.html/mdn.js
@@ -8,7 +8,7 @@ const today = () => {
     const month = currentDate.getMonth() + 1;
     const date = currentDate.getDate();
     const currenDate =new Date();
-    const formatteDate = '${year}年${month}月&{date}日';
+    const formatteDate = year + '年' + month + '月' + date +'日';
     console.log(formatteDate);
 
 };
